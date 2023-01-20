0eb9b78603faabde50f44085b542bc6eaaf23dac
matheus1985 <matheus.ifpb@gmail.com>
2023-01-20 09:40:50 -0300

    Imprimindo um nome como uma váriavel.

--- ex001.py
+++ ex001.py
@@ -1,4 +1,4 @@
 # imprimindo um nome com parametro
 
-nome = input('informe seu nome: ')
-print(f'prazer em te conhecer {nome}')
+nome = input('digite seu nome: ')
+print(f'prazer em te conhecer {nome}!')
