b7a384bfb31e1318eee30c1e6d9fc37fd36c0410
matheus1985 <matheus.ifpb@gmail.com>
2023-01-20 10:55:37 -0300

    Conversor de distancia

---
+++ ex008.py
@@ -0,0 +1,6 @@
+# Conversor de distancia
+
+d = int(input('informe uma distancia em metros: '))
+
+print(f'{d} metros correspondem a {d/1000}km, ou {d/100}hm, ou {d/10}dam, ou {d*10}dm, ou {d*100}cm ou {d*1000}mm')
+
