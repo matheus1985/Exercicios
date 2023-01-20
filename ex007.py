024f6a8c391836f9489d84ed2b48fbca0bdf3eff
matheus1985 <matheus.ifpb@gmail.com>
2023-01-20 10:40:06 -0300

    média de tres notas

--- ex007.py
+++ ex007.py
@@ -1,8 +1,8 @@
 import math
 #media de notas
 
-n1 = float(input('informe sua 1° nota: '))
-n2 = float(input(f'informe sua 2° nota: '))
-n3 = float(input(f'informe sua 3° nota: '))
+n1 = float(input('digite sua 1° nota: '))
+n2 = float(input(f'digite sua 2° nota: '))
+n3 = float(input(f'digite sua 3° nota: '))
 
 print(f'a média entre {n1:.1f}, {n2:.1f} e {n3:.1f} = {(n1+n2+n3)/3:.1f}')
