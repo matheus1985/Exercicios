ce66b7cec6d1be27140911b1155409f47dd1ffb5
matheus1985 <matheus.ifpb@gmail.com>
2023-01-20 09:47:06 -0300

    Somando dois numeros inteiros.

--- ex002.py
+++ ex002.py
@@ -1,5 +1,5 @@
 # somando 2 numeros
 
-n1 = int(input('informe um numero: '))
-n2 = int(input('informe outro numero: '))
+n1 = int(input('digite um numero inteiro: '))
+n2 = int(input('informe outro numero inteiro: '))
 print(f'a soma entre {n1} e {n2} é {n1+n2}')
