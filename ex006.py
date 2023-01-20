ecdb1dd9246f33b2e31e9b31945dcc522083e0a4
matheus1985 <matheus.ifpb@gmail.com>
2023-01-20 10:22:52 -0300

    Funções matemáticas de um numero

---
+++ ex006.py
@@ -0,0 +1,10 @@
+import math
+
+
+n = float(input('informe um numero inteiro: '))
+print(f'o dobro de {n} = {2*n}')
+print(f'{n} elevado ao quadrado = {math.pow(n,2)}')
+print(f'o triplo de {n} = {n*(math.pow(n,2))}')
+print(f'raiz de {n} = {math.sqrt(n)} ')
+
+
