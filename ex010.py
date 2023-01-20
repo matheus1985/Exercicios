4a653ed984975dbf1c570b8f2adc4f047a3b6086
matheus1985 <matheus.ifpb@gmail.com>
2023-01-20 11:08:37 -0300

    Conversor de real para dolar

---
+++ ex010.py
@@ -0,0 +1,6 @@
+#conversor de moeda
+
+valor = float(input('informe quanto voce tem em dinheiro: '))
+dolar = 5.23
+
+print(f'com R${valor} real(ais) voce pode comprar U${valor/dolar:.2f} dolar(es)')
