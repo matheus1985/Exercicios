c9263f85e9931ce352b2ce6e672f1a91d17a34ad
matheus1985 <matheus.ifpb@gmail.com>
2023-01-21 14:59:46 -0300

    Cálculo de desconto

---
+++ ex012.py
@@ -0,0 +1,5 @@
+# calculo de descontos
+
+valor = float(input('informe o preço do produto: '))
+
+print(f'o produto que custava {valor:.2f}, na promoção com desconto de 5% vai custar {valor*0.95:.2f}')
