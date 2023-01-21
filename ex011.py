9b1b70f3a15e76ec60ebf9a267895d0d397bdc03
matheus1985 <matheus.ifpb@gmail.com>
2023-01-21 14:52:22 -0300

    Calculo de tinta para pintar uma area

--- ex011.py
+++ ex011.py
@@ -4,4 +4,4 @@ largura = float(input('informe o largura da parede: '))
 comprimento = float(input('informe o comprimento da parede: '))
 
 print(f'sua parede possui uma área de {largura*comprimento}m²')
-print(f'para pintar sua parede voce vai precisar de {(largura*comprimento)/2:.2f} litros de tinta')
+print(f'para pintar sua parede voce vai precisar de {(largura*comprimento)/2:.2f}Lts de tinta')
