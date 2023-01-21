77e59f3ed1c4287a0011d3f0bd56436ba30df662
matheus1985 <matheus.ifpb@gmail.com>
2023-01-21 15:22:38 -0300

    Conversor de temperatura

--- ex014.py
+++ ex014.py
@@ -1,4 +1,4 @@
 #conversor de temperatura
 
 temp = float(input('informe a temperatura em C°: '))
-print(f'{temp}° celsius correspondem a {(temp*9/5)+32:.2f}° Fahrenheit')
+print(f'{temp}° Celsius correspondem a {(temp*9/5)+32:.2f}° Fahrenheit')
