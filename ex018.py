a4aa5866c943a5a65b1cdd48e4819da51bbbd07c
matheus1985 <matheus.ifpb@gmail.com>
2023-01-23 07:49:54 -0300

    Seno, Cosseno e Tangente

--- ex018.py
+++ ex018.py
@@ -1,7 +1,7 @@
 #Cálculo do seno, cosseno e tangente
 import math
 
-ang = float(input(f'informe o angulo em que  você deseja informações: '))
+ang = float(input(f'informe o angulo em que você deseja informações: '))
 ang = math.radians(ang)
 print(f'SENO de {ang}° = {math.sin(ang):.2f}')
 print(f'COSSENO de {ang}° = {math.cos(ang):.2f}')
